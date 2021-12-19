e254b6e49e34959d2223ed9f508d3eb4c7664e0d
김지수 <62272445+mogamogua@users.noreply.github.com>
2021-12-19 22:41:59 +0900

    first set

---
+++ next.config.js
@@ -0,0 +1,23 @@
+// const withImages = require('next-images')
+// module.exports = withImages()
+// const withCSS = require('@zeit/next-css');
+// module.exports = withCSS({});
+// module.exports = {
+//   images: {
+//     domains: ['http://localhost:8000'],
+//   },
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ["@svgr/webpack"],
+//     });
+//     return config;
+//   }
+// };
+
+module.exports = {
+  reactStrictMode: true, 
+  images: {
+    domains: [ 'localhost' ],
+  },
+}
