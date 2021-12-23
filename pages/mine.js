684a15c1842c629e29bf1939723b0f05831583be
Bean <whddkrak@gmail.com>
2021-12-24 00:12:20 +0900

    [ADD] mine loader

--- pages/mine.js
+++ pages/mine.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { check_auth_status } from "../redux/actions/auth";
 import QuestionList from "../components/questions/QuestionList";
 import { setMine, setPage, setMinePage, setQuestion} from "../modules/questions";
 import Prevent from '../components/questions/PreventRerender';
+import Loader from 'react-loader-spinner';
 
 function Mine() {
   const dispatch = useDispatch();
   const {data, count, page, smallCriteria, loading, error, mineToken} = useSelector((state) => state.questions);
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const [token, setToken] = useState(""); 
-
-  const test = useSelector((state) => state.questions);
   
   const getAccessToken = async () => {
     if (dispatch && dispatch !== null && dispatch !== undefined) {
@@ -30,10 +30,10 @@ function Mine() {
     getAccessToken();  
     dispatch(setMine(1, "all", token));  
   }, [dispatch, token]);
-  
+
   return (
     <Prevent>
-      {isAuthenticated ? (
+      {token && isAuthenticated ? (
         <>
           <QuestionList
             data={data}
@@ -41,9 +41,19 @@ function Mine() {
             count={count}
           ></QuestionList>
         </>
-      ) : null}
+      ) : <LoaderContainer><Loader type="Oval" color="#FFE59C" width={100} height={100}/></LoaderContainer> }
     </Prevent>
   );
 }
 
 export default React.memo(Mine);
+
+const LoaderContainer = styled.ul`
+  height: 100%;
+  border-radius: 0.125rem;
+  margin-top: 24px;
+  padding: 1.625rem 2.1875rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
