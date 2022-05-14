ba2f5402b83dc0865fe1b6a33f270129266cce6d
JisuKim <jso_osj@naver.com>
2022-05-14 22:16:02 +0900

    [FIX] search action함수 dispatch 수정

--- redux/actions/search.js
+++ redux/actions/search.js
@@ -9,49 +9,49 @@ import {
   SET_SEARCH_LOADING,
 } from "./types";
 
-export const setSearchLoading = (dispatch) => {
+export const setSearchLoading = () => (dispatch) => {
   dispatch({
     type: SET_SEARCH_LOADING,
   });
 };
 
-export const removeSearchLoading = (dispatch) => {
+export const removeSearchLoading = () => (dispatch) => {
   dispatch({
     type: REMOVE_SEARCH_LOADING,
   });
 };
 
-export const setNoSearchResult = (dispatch) => {
+export const setNoSearchResult = () => (dispatch) => {
   dispatch({
     type: NO_SEARCH_RESULT,
   });
 };
 
-export const resetNoSearchResult = (dispatch) => {
+export const resetNoSearchResult = () => (dispatch) => {
   dispatch({
     type: RESET_NO_SEARCH_RESULT,
   });
 };
 
-export const setSearchData = (dispatch) => {
+export const setSearchData = () => (dispatch) => {
   dispatch({
     type: SET_SEARCH_DATA,
   });
 };
 
-export const resetSearchData = (dispatch) => {
+export const resetSearchData = () => (dispatch) => {
   dispatch({
     type: RESET_SEARCH_DATA,
   });
 };
 
-export const enterSearchInput = (dispatch) => {
+export const enterSearchInput = () => (dispatch) => {
   dispatch({
     type: ENTER_SEARCH_INPUT,
   });
 };
 
-export const resetSearchInput = (dispatch) => {
+export const resetSearchInput = () => (dispatch) => {
   dispatch({
     type: RESET_SEARCH_INPUT,
   });
