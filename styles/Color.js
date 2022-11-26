1a0b61f3c16020c857981dab7e4ce040173e9194
Jisu Kim <jskim@elicer.com>
2022-11-26 17:00:07 +0900

    [CHORE]: add jsdoc comment to color.js

--- styles/Color.js
+++ styles/Color.js
@@ -1,14 +1,35 @@
+/**
+ *
+ * - `gray1`: `#F7F7F7`,
+ * - `gray2`: `#ECECEC`,
+ * - `gray3`: `#C4C4C4`,
+ * - `gray4`: `#A4A4A4`,
+ * - `gray5`: `#7A7A7A`,
+ * - `gray6`: `#5F5F5F`,
+ * - `gray7`: `#4A4A4A`,
+ * - `mildBlack`: `#343434`,
+ * - `bgWhite`: `#FFFEFC`,
+ * - `mainYellow`: `#FFD358`,
+ * - `gageYellow`: `#FFE28F`,
+ * - `yellowGray0`: `#F1EFE9`,
+ * - `yellowGray1`: `#E9E7DE`,
+ * - `yellowGray2`: `#D3D1C7`,
+ */
 export const colors = {
-  gary1: "#F7F7F7",
-  gary2: "#ECECEC",
-  gary3: "#C4C4C4",
-  gary4: "#A4A4A4",
-  gary5: "#7A7A7A",
-  gary6: "#5F5F5F",
-  gary7: "#4A4A4A",
+  gray1: "#F7F7F7",
+  gray2: "#ECECEC",
+  gray3: "#C4C4C4",
+  gray4: "#A4A4A4",
+  gray5: "#7A7A7A",
+  gray6: "#5F5F5F",
+  gray7: "#4A4A4A",
 
   mildBlack: "#343434",
   bgWhite: "#FFFEFC",
   mainYellow: "#FFD358",
   gageYellow: "#FFE28F",
+
+  yellowGray0: "#F1EFE9",
+  yellowGray1: "#E9E7DE",
+  yellowGray2: "#D3D1C7",
 };
